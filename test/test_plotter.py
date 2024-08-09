0fd7728a7c0384cbb285da400cbe299f64901834
dongyeop3813 <dywoo1247@gmail.com>
2024-08-09 15:19:42 +0900

    Refactor test code

--- test/test_plotter.py
+++ test/test_plotter.py
@@ -6,7 +6,7 @@ from energy import Plotter, GMM25, GMM9, Funnel, ManyWell
 FIGURE_PATH = "./test/figure/"
 
 
-def test_plotter_make_plot_for_gmm25():
+def test_plotter_make_sample_plot_for_gmm25():
     energy_function = GMM25(device="cuda:0", dim=2)
     plotter = Plotter(
         energy_function=energy_function, fig_size=(12, 7), plotting_bounds=(-20, 20)
@@ -14,21 +14,21 @@ def test_plotter_make_plot_for_gmm25():
 
     sample = torch.randn((100, 2), device="cuda:0")
 
-    fig, _ = plotter.make_plot(sample)
+    fig, _ = plotter.make_sample_plot(sample)
     fig.savefig(FIGURE_PATH + "gmm25_test_figure.png")
 
 
-def test_plotter_make_plot_for_gmm9():
+def test_plotter_make_sample_plot_for_gmm9():
     energy_function = GMM9(device="cuda:0", dim=2)
     plotter = Plotter(energy_function=energy_function, fig_size=(12, 7))
 
     sample = torch.randn((100, 2), device="cuda:0")
 
-    fig, _ = plotter.make_plot(sample)
+    fig, _ = plotter.make_sample_plot(sample)
     fig.savefig(FIGURE_PATH + "gmm9_test_figure.png")
 
 
-def test_plotter_make_plot_for_manywell():
+def test_plotter_make_sample_plot_for_manywell():
     energy_function = ManyWell(device="cuda:0", dim=32)
 
     # Ground truth sample plot
@@ -41,11 +41,11 @@ def test_plotter_make_plot_for_manywell():
         plotting_bounds=(-3, 3),
     )
 
-    fig, _ = plotter.make_plot(sample)
+    fig, _ = plotter.make_sample_plot(sample)
     fig.savefig(FIGURE_PATH + "manywell_test_figure.png")
 
 
-def test_plotter_make_plot_for_funnel():
+def test_plotter_make_sample_plot_for_funnel():
     energy_function = Funnel(device="cuda:0", dim=3)
 
     # Ground truth sample plot
@@ -57,7 +57,7 @@ def test_plotter_make_plot_for_funnel():
         projection_dims=[(0, 1), (1, 2)],
     )
 
-    fig, _ = plotter.make_plot(sample)
+    fig, _ = plotter.make_sample_plot(sample)
     fig.savefig(FIGURE_PATH + "funnel_test_figure.png")
 
 
